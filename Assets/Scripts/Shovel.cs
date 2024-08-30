becfe02473fa4ee457ef08d2057ed4d3f9b0d57a
Grafiszti <grafiszti@gmail.com>
2024-08-31 01:06:01 +0200

    shit works

--- Assets/Scripts/Shovel.cs
+++ Assets/Scripts/Shovel.cs
@@ -5,10 +5,10 @@ public class Shovel : MonoBehaviour
 {
     public float damage = 10.0f;      // Damage dealt by the pickaxe
     public float attackRange = 7.0f;  // Range of the pickaxe attack
-    public float attackRate = 1.0f;   // per 1 second
+    public float attackRate = 10.1f;   // per 1 second
     public Camera playerCamera;       // Reference to the player's camera
 
-    private float nextAttackTime = 0.1f;
+    private float nextAttackTime = 0.0f;
     private Animator animator;
 
     void Start()
@@ -29,16 +29,24 @@ public class Shovel : MonoBehaviour
     {
         RaycastHit hit;
         Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.forward * attackRange, Color.red, 3.0f);
-        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward * attackRange, out hit, attackRange))
+        bool raycastHit = Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward * attackRange, out hit, attackRange);
+        if (raycastHit)
         {
-            Zombie enemy = hit.transform.GetComponent<Zombie>();
-            Debug.Log($"Hit: {hit.transform.name}");
-            animator.SetTrigger("Attack");
-            Vector3 pushDirection = hit.point - playerCamera.transform.position;
-            if (enemy != null)
+            if (hit.transform.CompareTag("Zombie"))
             {
-                enemy.TakeHit(damage, pushDirection);
+                Zombie enemy = hit.transform.GetComponent<Zombie>();
+                Debug.Log($"Hit: {hit.transform.name}");
+                animator.SetTrigger("Attack");
+                if (enemy != null)
+                {
+                    enemy.TakeHit(damage, hit.point - playerCamera.transform.position);
+                }
             }
+            else
+            {
+                animator.SetTrigger("AttackMiss");
+            }
+
         }
         else
         {
