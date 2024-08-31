55a6886ac661f82493f3d3a9f26522e0ffbf6492
Grafiszti <grafiszti@gmail.com>
2024-08-31 14:27:47 +0200

    spawner

--- Assets/Scripts/Grave.cs
+++ Assets/Scripts/Grave.cs
@@ -8,36 +8,10 @@ public class Grave : MonoBehaviour
 {
     // Start is called before the first frame update
     public float graveRespawnTime = 10.0f;
+    public float graveSpawnDelay = 10.0f;
+    public float graveMaxSpawnDelay = 10.0f;
     public float graveMaxRespawnTime = 10.0f;
     public float graveDigTime = 2.0f;
-
-    private Transform zombiesRoot;
-
-    public Zombie zombiePrefab;
-    void Start()
-    {
-        
-        zombiesRoot = GameObject.Find("Zombies").transform;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if (graveRespawnTime > 0)
-        {
-            graveRespawnTime -= Time.deltaTime;
-            if (graveRespawnTime <= 0)
-            {
-                Zombie newObject = Instantiate(zombiePrefab, GetComponent<Transform>().position, GetComponent<Transform>().rotation);
-                newObject.transform.parent = this.zombiesRoot;
-            }
-        }
-        else
-        {
-            graveRespawnTime = graveMaxRespawnTime;
-        }
-    }
 
     public void Dig()
     {
--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ZombieSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private List<Grave> graves;
+
+    public int maxZombies = 8;
+    private int zombiesCounter = 0;
+    private float delay = 5.0f;
+
+    private Transform zombiesRoot;
+
+    public Zombie zombiePrefab;
+
     void Start()
     {
-        
+        zombiesRoot = GameObject.Find("Zombies").transform;
+        graves = new List<Grave>(GameObject.FindObjectsOfType<Grave>());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        zombiesCounter = new List<Zombie>(GameObject.FindObjectsOfType<Zombie>()).Count;
+        delay -= Time.deltaTime;
+
+        if (delay <= 0 && zombiesCounter < maxZombies)
+        {
+            delay = 5.0f;
+            SpawnZombie(graves[Random.Range(0, graves.Count)]);
+        }
+    }
+
+    void SpawnZombie(Grave grave)
+    {
+        Zombie newObject = Instantiate(zombiePrefab, grave.GetComponent<Transform>().position, grave.GetComponent<Transform>().rotation);
+        newObject.transform.parent = this.zombiesRoot;
     }
 }
